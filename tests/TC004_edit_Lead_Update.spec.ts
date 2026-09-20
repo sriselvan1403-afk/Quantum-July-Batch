6cf807e4a9b31ea8b2d7d99bdb7d896269a4d349
Santhosh Sri <sriselvan1403@gmail.com>
2026-09-20 16:14:54 +0530

    Changed TC001  - Created TC004

--- tests/TC004_edit_Lead_Update.spec.ts
+++ tests/TC004_edit_Lead_Update.spec.ts
@@ -1,23 +1,21 @@
-import { expect, test } from '../customFixtures/crmFixtures.ts';
+import { expect, test } from '../customFixtures/crmFixtures';
 import { ExcelReader } from '../utils/excelUtils';
 import { FakerData } from '../utils/fakerUtils';
 import { appConstants } from '../constants/appConstants';
-import path from 'path';
- 
 
-test('Add New Contact and Verify', async ({ addContactDetailsPage, contactsLeadsPage }) => {
+
+test('Edit Contact and Verify', async ({ addContactDetailsPage, contactsLeadsPage }) => {
     test.info().annotations.push(
-        { type: 'Author', description: 'Arpitha' },
-        { type: 'TestCase', description: 'Add new contact and verify its displayed' },
-        { type: 'Test Description', description: "Verifying newly created contact is displayed in contact leads page" }
+        { type: 'Author', description: 'Vishnu' },
+        { type: 'TestCase', description: 'Edit contact and verify its updated' },
+        { type: 'Test Description', description: "Verifying edited contact details are updated in contact leads page" }
     );
 
-     const filePath = path.resolve(__dirname, '../resources/testdata.xlsx');
-    const excelReader = new ExcelReader(filePath);
-    const excelData = excelReader.getRowByTestcase('contact', 'TC_ID', 'TC_001');
+    const excelReader = new ExcelReader('.../resources/testdata.xlsx');
+    const excelData = excelReader.getRowByTestcase('contact', 'TC_ID', 'TC_002');
 
     const contactName = FakerData.getFirstName();
-    await addContactDetailsPage.enterName("Automate");
+    await addContactDetailsPage.enterName(contactName);
     await addContactDetailsPage.enterEmail(FakerData.getEmail());
     await addContactDetailsPage.enterPhoneNumber(excelData.PHONE_NUMBER);
     await addContactDetailsPage.enterCompany(FakerData.company());
@@ -34,7 +32,11 @@ test('Add New Contact and Verify', async ({ addContactDetailsPage, contactsLeads
     await addContactDetailsPage.clickAddButton();
 
     const contactsPage = await contactsLeadsPage.switchBackToContactsPage();
+
+    // const actualContactName = await contactsPage.getFirstContactName();
+    // expect(actualContactName).toContain(contactName);
+
     await expect(await contactsPage.getFirstContactName()).toHaveText(contactName);
- 
+    //await page.pause();
 });
 
